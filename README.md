4611581747000a90cb3aed765cced222d35769e6
neuraxislink <282602809@qq.com>
2022-10-03 12:11:19 +0800

    猜数游戏
    比较大小

---
+++ README.md
@@ -0,0 +1,34 @@
+# Rust Samples 
+The Rust Programming Language
+
+# Ref
+语言入门
+lang=cn
+https://kaisery.github.io/trpl-zh-cn/
+
+lang=en
+https://doc.rust-lang.org/stable/book/
+
+类库
+https://docs.rs/
+## 安装:
+> https://www.rust-lang.org
+> 
+> Linux or Mac build :
+> 
+> curl https//sh.rustup.rs -sSf | sh
+
+ 更新rust
+> rustup update
+
+删除rust
+> rustup self uninstall
+
+安装验证
+> rustc -version
+
+本地文档
+> rustup doc
+
+
+
---
+++ s2_getnumbergame/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::{thread_rng, Rng};//trait 就是其他语言的接口
+fn main() {
+
+    println!("猜数游戏");
+    let secret_number =    thread_rng().gen_range(1,101);
+    println!("神秘数字是{}",secret_number);
+    println!("猜一个数");
+    let mut  guess =String::new();
+    io::stdin().read_line(&mut guess).expect("无法读取行");
+    let guess:u32 =guess.trim().parse().expect("Please type a number!");// ln
+
+    println!("你猜测的数是:{}",guess);
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("太小"),
+        Ordering::Greater => println!("太大"),
+        Ordering::Equal => println!("等于"),
+    }
+}
